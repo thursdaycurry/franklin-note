9cfe00e79d9c0fe87a2173c92734d9ee2c8ea4d3
thursdaycurry <thursdaycurry@gmail.com>
2023-11-06 01:20:08 +0900

    edit process text in homepage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,4 +1,17 @@
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <h2>Welcome to Franklin Note</h2>
+      <div>Process</div>
+      <ol>
+        <li>
+          <a href="/note">Note</a> Page로 간다
+        </li>
+        <li>마음에 드는 영어 문장의 선택하거나 추가한다</li>
+        <li>영어 문장을 variation 한다</li>
+        <li>여러분의 멋진 문장을 이메일로 보내면 끝!</li>
+      </ol>
+    </div>
+  );
 };
 export default HomePage;
